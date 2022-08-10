e1ef8da3dcce48ce5aeb2e6f9f7f5deb6a941bd0
Niki Hite <nikihite@comcast.net>
2022-08-10 11:48:32 -0700

    protected should return a 401 if not authenticated

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -45,6 +45,11 @@ describe('backend-express-template routes', () => {
     expect(res.status).toEqual(200);
   });
 
+  it('/protected should return a 401 if not authenticated', async () => {
+    const res = await request(app).get('/api/v1/users/protected');
+    expect(res.status).toEqual(401);
+  });
+
   afterAll(() => {
     pool.end();
   });
